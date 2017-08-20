f7ac689d20dbe9386e5c3f9154838f18df02437c
fm-labs <flowmotionlabs@gmail.com>
2017-08-20 19:29:15 +0200

    renamed ActionComponent events; attached Controller listeners to global event manager to autoload actions; updated plugin backends

--- config/form_horizontal_templates.php
+++ config/form_horizontal_templates.php
@@ -3,17 +3,17 @@ return [
     //'input' => 'input type="{{type}}" name="{{name}}"{{attrs}}/>',
     //'select' => '<select name="{{name}}"{{attrs}}>{{content}}</select>',
 
-    'formGroup' => '<div class="col-sm-3 col-md-4">{{label}}</div><div class="col-sm-9 col-md-8">{{input}}</div>',
+    'formGroup' => '<div class="col-sm-3 col-md-4 col-lg-5">{{label}}</div><div class="col-sm-9 col-md-8 col-lg-7">{{input}}</div>',
 
     'checkbox' => '<input type="checkbox" name="{{name}}" value="{{value}}"{{attrs}}>',
     'checkboxFormGroup' => '{{label}}',
     'checkboxWrapper' => '<div class="checkbox-wrapper">{{label}}</div>',
-    'nestingLabel' => '<div class="col-sm-3 col-md-4">{{hidden}}<label{{attrs}}>{{text}}</label></div><div class="col-sm-9 col-md-8">{{input}}</div>',
+    'nestingLabel' => '<div class="col-sm-3 col-md-4 col-lg-5">{{hidden}}<label{{attrs}}>{{text}}</label></div><div class="col-sm-9 col-md-8 col-lg-7">{{input}}</div>',
 
     'radioContainer' => '<div class="form-group input-radio">{{content}}',
-    'radioFormGroup' => '{{label}}<div class="col-sm-9 col-md-8">{{input}}</div>',
+    'radioFormGroup' => '{{label}}<div class="col-sm-9 col-md-8 col-lg-7">{{input}}</div>',
     'radioWrapper' => '<div class="radio">{{label}}</div>',
 
     'multicheckboxContainer' => '<div class="form-group {{required}}">{{content}}</div>',
-    'multicheckboxFormGroup' => '{{label}}<div class="multicheckbox-formgroup col-sm-9 col-md-8">{{input}}</div>'
+    'multicheckboxFormGroup' => '{{label}}<div class="multicheckbox-formgroup col-sm-9 col-md-8 col-lg-7">{{input}}</div>'
 ];
