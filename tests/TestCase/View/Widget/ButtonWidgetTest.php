05813a4daa5b2fd29a44bc4c6a896998854e2fc4
fm-labs <flowmotionlabs@gmail.com>
2018-09-15 19:13:53 +0200

    fixed tests

--- tests/TestCase/View/Widget/ButtonWidgetTest.php
+++ tests/TestCase/View/Widget/ButtonWidgetTest.php
@@ -15,6 +15,17 @@ class ButtonWidgetTest extends TestCase
 {
 
     /**
+     * @var StringTemplate
+     */
+    public $templates;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    public $context;
+
+
+    /**
      * {@inheritDoc}
      */
     public function setUp()
@@ -29,30 +40,46 @@ class ButtonWidgetTest extends TestCase
 
     /**
      * Test render default button.
+     * @return void
      */
     public function testRender()
     {
         $button = new ButtonWidget($this->templates);
         $result = $button->render([], $this->context);
 
-        $expected = '<button class="btn" type="submit"></button>';
+        $expected = '<button class="btn"></button>';
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Test render submit button.
+     * @return void
+     */
+    public function testRenderSubmit()
+    {
+        $button = new ButtonWidget($this->templates);
+        $result = $button->render(['type' => 'submit'], $this->context);
+
+        $expected = '<button type="submit" class="btn btn-primary"></button>';
         $this->assertEquals($expected, $result);
     }
 
     /**
      * Test render with button text.
+     * @return void
      */
     public function testRenderWithText()
     {
         $button = new ButtonWidget($this->templates);
         $result = $button->render(['text' => 'Test'], $this->context);
 
-        $expected = '<button class="btn" type="submit">Test</button>';
+        $expected = '<button class="btn">Test</button>';
         $this->assertEquals($expected, $result);
     }
 
     /**
      * Test render with button text.
+     * @return void
      */
     public function testRenderType()
     {
@@ -65,49 +92,51 @@ class ButtonWidgetTest extends TestCase
 
     /**
      * Test render with button text.
+     * @return void
      */
     public function testRenderWithAttributes()
     {
         $button = new ButtonWidget($this->templates);
         $result = $button->render(['text' => 'Test', 'data-test' => 'foo'], $this->context);
 
-        $expected = '<button data-test="foo" class="btn" type="submit">Test</button>';
+        $expected = '<button data-test="foo" class="btn">Test</button>';
         $this->assertEquals($expected, $result);
     }
 
     /**
      * Test render default button.
+     * @return void
      */
     public function testRenderWithBuiltInClassAttributes()
     {
         $button = new ButtonWidget($this->templates);
 
         $result = $button->render(['class' => 'success'], $this->context);
-        $expected = '<button class="btn btn-success" type="submit"></button>';
+        $expected = '<button class="btn btn-success"></button>';
         $this->assertEquals($expected, $result);
 
         $result = $button->render(['class' => 'default'], $this->context);
-        $expected = '<button class="btn btn-default" type="submit"></button>';
+        $expected = '<button class="btn btn-default"></button>';
         $this->assertEquals($expected, $result);
 
         $result = $button->render(['class' => 'danger'], $this->context);
-        $expected = '<button class="btn btn-danger" type="submit"></button>';
+        $expected = '<button class="btn btn-danger"></button>';
         $this->assertEquals($expected, $result);
 
         $result = $button->render(['class' => 'warning'], $this->context);
-        $expected = '<button class="btn btn-warning" type="submit"></button>';
+        $expected = '<button class="btn btn-warning"></button>';
         $this->assertEquals($expected, $result);
 
         $result = $button->render(['class' => 'info'], $this->context);
-        $expected = '<button class="btn btn-info" type="submit"></button>';
+        $expected = '<button class="btn btn-info"></button>';
         $this->assertEquals($expected, $result);
 
         $result = $button->render(['class' => 'link'], $this->context);
-        $expected = '<button class="btn btn-link" type="submit"></button>';
+        $expected = '<button class="btn btn-link"></button>';
         $this->assertEquals($expected, $result);
 
         $result = $button->render(['class' => 'foo'], $this->context);
-        $expected = '<button class="btn foo" type="submit"></button>';
+        $expected = '<button class="btn foo"></button>';
         $this->assertEquals($expected, $result);
     }
 }
