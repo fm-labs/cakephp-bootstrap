91c7b21be923e6084f33054f779c5aae2d449a68
fm-labs <flowmotionlabs@gmail.com>
2019-03-08 17:39:13 +0100

    added BootstrapHelper

---
+++ src/View/Helper/BootstrapHelper.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bootstrap\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Bootstrap helper
+ */
+class BootstrapHelper extends Helper
+{
+
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize(array $config)
+    {
+        //$this->_View->Html->css();
+        //$this->_View->Html->script();
+    }
+}
