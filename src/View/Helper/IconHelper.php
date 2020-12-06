91a4c1f59ba1c39fe3d29940cda801360d32ebbf
fm-labs <flowmotionlabs@gmail.com>
2020-12-06 21:47:38 +0100

    minor fixes

--- src/View/Helper/IconHelper.php
+++ src/View/Helper/IconHelper.php
@@ -6,35 +6,84 @@ namespace Bootstrap\View\Helper;
 use Cake\View\Helper;
 use Cake\View\StringTemplateTrait;
 
+/**
+ * Class IconHelper
+ *
+ * @package Bootstrap\View\Helper
+ * @method glyphicon(string $icon, array $options = []): string
+ * @method fontawesome(string $icon, array $options = []): string
+ */
 class IconHelper extends Helper
 {
     use StringTemplateTrait;
 
     protected $_defaultConfig = [
+        'defaultTemplate' => 'fontawesome',
         'templates' => [
-            // 'icon' => '<span class="glyphicon glyphicon-{{class}}"{{attrs}}></span>', #bootstrap style
-            'icon' => '<i class="fa fa-{{class}}"{{attrs}}></i>', # fontawesome style
+            'icon_glyphicon' => '<span class="glyphicon glyphicon-{{class}}"{{attrs}}></span>', #bootstrap style
+            'icon_fontawesome' => '<i class="fa fa-{{class}}"{{attrs}}></i>', # fontawesome style
         ],
     ];
 
     public $helpers = ['Html'];
 
     /**
-     * @param $class
-     * @param array $options
+     * @param string $icon Icon name
+     * @param array $options Icon options
      * @return null|string
      */
-    public function create($class, $options = [])
+    public function create(string $icon, array $options = []): string
     {
-        $options += ['tag' => 'icon', 'class' => '', 'attrs' => []];
+        $options += ['template' => null, 'class' => null, 'attrs' => []];
 
-        $tag = $options['tag'];
-        unset($options['tag']);
+        $template = $options['template'] ?? $this->getConfig('defaultTemplate');
+        $class = trim(sprintf('%s %s', $icon, (string)$options['class']));
 
-        if (isset($options['class'])) {
-            $options['class'] = sprintf("%s %s", $class, $options['class']);
+        return $this->templater()->format('icon_' . $template, [
+            'class' => $class,
+            'attrs' => $this->templater()->formatAttributes($options['attrs']),
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __call($method, $params)
+    {
+        if (isset($this->_config['templates']['icon_' . $method]) && count($params) >= 1) {
+            $icon = $params[0];
+            $options = $params[1] ?? [];
+            $options['template'] = $method;
+
+            return $this->create($icon, $options);
         }
 
-        return $this->templater()->format($tag, $options);
+        parent::__call($method, $params);
     }
+
+//    /**
+//     * Create fontawesome icon.
+//     *
+//     * @param string $icon Icon name
+//     * @param array $options Icon options
+//     * @return string
+//     */
+//    public function fontawesome(string $icon, array $options = []): string
+//    {
+//        $options['template'] = 'fontawesome';
+//        return $this->create($icon, $options);
+//    }
+//
+//    /**
+//     * Create glyphicon icon.
+//     *
+//     * @param string $icon Icon name
+//     * @param array $options Icon options
+//     * @return string
+//     */
+//    public function glyphicon(string $icon, array $options = []): string
+//    {
+//        $options['template'] = 'glyphicon';
+//        return $this->create($icon, $options);
+//    }
 }
